3a68d1731c6ba8e98fd2bf5ce7611ad2dcea0a8d
Shannon Rice <srice9036@gmail.com>
2023-04-01 12:19:32 +0100

    [#3] {t:30} [Hashsalt] Refactored, added docustrings and updated tests for hashsalt class

--- hash/hashsalt.py
+++ hash/hashsalt.py
@@ -1,11 +1,18 @@
+'''A module that uses haslib and salting to encode a string'''
 import os
 import hashlib
 
 class HashSalt():
-    '''Generates a hashed and salted version of the string provided when the class is instantiated'''
-    def __init__(self, to_hash: str) -> None:
-        '''Generates random bytes for the salt and assigns the encoded version of the input string to a variable'''
-        self._salt = os.urandom(32)
+    '''Generates a hashed and salted version
+       of the string provided when the class is instantiated'''
+    def  __init__(self, to_hash: str , salt = None) -> None:
+        '''Generates random bytes for the salt and
+           assigns the encoded version of the input string to a variable'''
+        if salt is None:
+            self._salt = os.urandom(32)
+        else:
+            self._salt = salt
+
         self._key = to_hash.encode("utf-8")
 
     def generate_salted_hash(self) -> str:
--- tests/test_hashsalt.py
+++ tests/test_hashsalt.py
@@ -1,6 +1,6 @@
-from hash.hashsalt import HashSalt
 import random
 import string
+from hash.hashsalt import HashSalt
 
 def test_hashsalt() -> None:
     test_str = ''.join((random.choice(string.ascii_letters + string.digits) for i in range(8)))
@@ -8,14 +8,14 @@ def test_hashsalt() -> None:
     hashed_data = hash.generate_salted_hash()
 
     assert hashed_data == hash.generate_salted_hash()
-    
+
     hash1 = HashSalt(test_str)
     assert hashed_data != hash1.generate_salted_hash()
-    
+
     test_str = ''.join((random.choice(string.ascii_letters) for i in range(8)))
     hash2 = HashSalt(test_str)
     hashed = hash2.generate_salted_hash()
     assert hashed == hash2.generate_salted_hash()
-    
+
     hash3 = HashSalt(test_str)
     assert hashed != hash3.generate_salted_hash()
