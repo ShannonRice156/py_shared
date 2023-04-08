918445c37d39391b3df59af5b6efce9f001cb975
Shannon Rice <srice9036@gmail.com>
2023-04-08 20:35:53 +0100

    [#3] {t:30} [HashSalt] Updated docustring and added getter and setter to HashSalt

--- hash/hashsalt.py
+++ hash/hashsalt.py
@@ -9,12 +9,22 @@ class HashSalt():
         '''Generates random bytes for the salt and
            assigns the encoded version of the input string to a variable'''
         if salt is None:
-            self._salt = os.urandom(32)
+            self.salt = os.urandom(32)
         else:
-            self._salt = salt
+            self.salt = salt
 
         self._key = to_hash.encode("utf-8")
 
     def generate_salted_hash(self) -> str:
         '''Returns the the _key salted and hashed'''
-        return hashlib.pbkdf2_hmac('sha256', self._key, self._salt, 1000)
+        return hashlib.pbkdf2_hmac('sha256', self._key, self.salt, 1000)
+
+    @property
+    def salt(self) -> bytes:
+        '''Returns the the _salt'''
+        return self._salt
+
+    @salt.setter
+    def salt(self, value: bytes) -> bytes:
+        '''Sets value of _salt'''
+        self._salt = value
--- tests/test_hashsalt.py
+++ tests/test_hashsalt.py
@@ -1,13 +1,14 @@
 import random
 import string
-from hash.hashsalt import HashSalt
+from ..hash.hashsalt import HashSalt
 
 def test_hashsalt() -> None:
+    '''Testing functionality of Hashsalt class'''
     test_str = ''.join((random.choice(string.ascii_letters + string.digits) for i in range(8)))
-    hash = HashSalt(test_str)
-    hashed_data = hash.generate_salted_hash()
+    hash_obj = HashSalt(test_str)
+    hashed_data = hash_obj.generate_salted_hash()
 
-    assert hashed_data == hash.generate_salted_hash()
+    assert hashed_data == hash_obj.generate_salted_hash()
 
     hash1 = HashSalt(test_str)
     assert hashed_data != hash1.generate_salted_hash()
